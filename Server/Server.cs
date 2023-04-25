6f05614cd9a43101a8e2e168cb4dd110d6c134ba
gyulim5 <ohrbfla1004@khu.ac.kr>
2023-04-25 16:31:41 +0900

    add Server

---
+++ Server/Server.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+namespace Server
+{
+    class Server
+    {
+        public static void Main(string[] args)
+        {
+            UDPServer server = new UDPServer(8080);
+            server.Start();
+
+            Console.ReadLine();
+        }
+    }
+}
